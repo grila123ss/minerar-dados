469828e9205c16d6b270236fbdd98a124f33d990
grila123ss <161061693+grila123ss@users.noreply.github.com>
2024-09-30 16:29:28 -0300

    Add files via upload

---
+++ graficos/informacoesglobais.js
@@ -0,0 +1,22 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+
+async function vizualizarInformacaoGlobais(){
+
+  const res = await fetch(url)
+  const dados = await res.json()
+  console.log(dados);
+
+
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+
+paragrafo.innerHTML = `kkk noseioqdizer
+${dados.total_pessoas_conectadas}`
+
+const container = document.getElementById('graficos-container')
+
+container.appendChild(paragrafo)
+
+}
+
+ vizualizarInformacaoGlobais();
